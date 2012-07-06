d88729e2c7f32dccc75a89fbfdc201079b460831
Manu Sporny <msporny@digitalbazaar.com>
2012-07-06 15:36:17 -0400

    Added call to create profile on payswarm authority.

--- tests/index.js
+++ tests/index.js
@@ -7,7 +7,8 @@ var program = require('commander');
 var payswarm = require('payswarm-client');
 var winston = require('winston');
 var util = require('util');
-var payswarmTools = require('payswarm.tools');
+var payswarmTools = require('../lib/payswarm-auth/payswarm.tools');
+var request = require('request');
 
 var loadTester = {};
 config = {};
@@ -15,12 +16,9 @@ config = {};
 // setup the logging framework
 var logger = new (winston.Logger)({
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'testing.log' })
-  ],
-  exceptionHandlers: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: 'testing-exceptions.log' })
+    new winston.transports.Console({timestamp: true}),
+    new winston.transports.File({
+      json: false, timestamp: true, filename: 'testing.log'})
   ]
 });
 
@@ -38,13 +36,16 @@ loadTester.run = function() {
     .option('--purchases <num>',
       'The number of purchases to perform per profile/identity (default: 1)',
       Number)
+    .option('--batch-size <num>',
+      'The number of profiles/listings to create at a time (default: 10)',
+      Number)
     .parse(process.argv);
 
   // initialize the configuration
   config.profiles = program.profiles || 1;
   config.listings = program.listings || 1;
   config.purchases = program.purchases || 1;
-  //config.vendorKey =
+  config.batchSize = program.batchSize || 10;
 
   // dump out the configuration
   logger.info('Config:', config);
@@ -56,14 +57,14 @@ loadTester.run = function() {
     createProfiles: function(callback) {
       logger.info(util.format('Creating %d profiles...', config.profiles));
       // FIXME: don't use arrays for this
-      async.forEach(new Array(config.profiles), batchSize,
+      async.forEachLimit(new Array(config.profiles), config.batchSize, 
         function(item, callback) {
           _createProfile(profiles, callback);
       }, callback);
     },
     createListings: function(callback) {
       logger.info(util.format('Creating %d listings...', config.listings));
-      async.forEachLimit(new Array(config.listings), batchSize,
+      async.forEachLimit(new Array(config.listings), config.batchSize,
         function(item, callback) {
           _createListing(listings, callback);
       }, callback);
@@ -83,31 +84,78 @@ loadTester.run = function() {
   });
 };
 
+// log uncaught exception and exit
+process.on('uncaughtException', function(err) {
+  logger.error(
+    err.toString(), err.stack ? {stack: err.stack} : null);
+  process.removeAllListeners('uncaughtException');
+  process.exit();
+});
+
 // run the program
 loadTester.run();
 
 /**
  * Creates a profile.
  *
+ * @param profiles the list of profiles to append to. 
  * @param callback(err) called once the operation completes.
  */
-function _createProfile(callback) {
-  var profile = {};
+function _createProfile(profiles, callback) {
+  var md = crypto.createHash('md5');
+  md.update(payswarmTools.uuid(), 'utf8');
+  var id = md.digest('hex').substr(12);
+  var email = 'patest-' + id + '@digitalbazaar.com';
 
-  logger.info(util.format('Profile: %j', profile));
-  profiles.push(profile);
-
-  callback(null);
+  // setup the profile creation template
+  var profileTemplate = {
+    '@context': 'http://purl.org/payswarm/v1',
+    email: email,
+    psaPassword: 'password',
+    psaIdentity: {
+      type: 'ps:PersonalIdentity',
+      psaSlug: 'patest-' + id,
+      label: 'PaySwarm Authority Test Identity'
+    },
+    account: {
+      psaSlug: 'primary',
+      label: 'Primary Account'
+    }
+  };
+  
+  // create the profile 
+  request.post({
+      url: 'https://payswarm.dev:19443/test/profile/create',
+      json: profileTemplate
+    },
+    function(err, response, body) {
+      if(!err && response.statusCode >= 400) {
+        err = JSON.stringify(body, null, 2);
+      }
+      if(err) {
+        logger.error('Failed to create profile: ', err.toString());
+        return callback(err);
+      }
+      
+      var profile = body;
+      logger.info('Profile: ' + JSON.stringify(profile, null, 2));
+      profiles.push(profile);
+      callback(null);
+    }
+  );
 }
 
 /**
  * Creates a listing.
  *
+ * @param listings the list of listings to append to. 
  * @param callback(err, listing) called once the operation completes.
  */
-function _createListing(callback) {
-  var uuid = payswarmTools.uuid();
-  var baseUrl = 'http://listings.dev.payswarm.com/payswarm-auth-tests/' + uuid;
+function _createListing(listings, callback) {
+  var md = crypto.createHash('md5');
+  md.update(payswarmTools.uuid(), 'utf8');
+  var id = md.digest('hex').substr(12);
+  var baseUrl = 'http://listings.dev.payswarm.com/payswarm-auth-tests/' + id;
   var assetId = baseUrl + '#asset';
   var listingId = baseUrl + '#listing';
 
@@ -118,11 +166,16 @@ function _createListing(callback) {
     creator: {
       fullName: 'PaySwarm Test Software'
     },
-    title : 'Test Asset ' + uuid,
+    title : 'Test Asset ' + id,
     assetContent: assetId,
     assetProvider: "https://payswarm.dev:19443/i/vendor",
   };
 
+  // generate the listing validity dates
+  var validFrom = new Date();
+  var validUntil = new Date();
+  validUntil.setFullYear(validFrom.getFullYear() + 1);
+  
   // generate the listing
   var listing = {
     id: listingId,
@@ -134,7 +187,7 @@ function _createListing(callback) {
       payeePosition: 0,
       payeeRate: '0.0500000',
       payeeRateType: 'com:FlatAmount',
-      comment: 'Payment for Asset ' + uuid + '.'
+      comment: 'Payment for Asset ' + id + '.'
     }],
     payeeRule : [{
       type: 'com:PayeeRule',
@@ -147,11 +200,12 @@ function _createListing(callback) {
     assetHash: '',
     license: 'http://purl.org/payswarm/licenses/blogging',
     licenseHash: 'ad8f72fcb47e867231d957c0bffb4c02d275926a',
-    validFrom: '2012-07-05T20:35:21+00:00',
-    validUntil: '2012-07-06T20:35:21+00:00',
+    validFrom: validFrom,
+    validUntil: validUntil,
   };
-  logger.info(util.format('Listing: %j', listing));
+  logger.info('Listing: ' + JSON.stringify(listing, null, 2));
 
+  /*
   async.waterfall([
     function(callback) {
       payswarm.hash(asset, callback);
@@ -169,4 +223,5 @@ function _createListing(callback) {
     }
     callback(err);
   });
+  */
 }

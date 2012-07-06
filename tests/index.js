04c9cc04ae444060f04761ec3c80c763c1557360
Dave Longley <dlongley@digitalbazaar.com>
2012-07-06 12:13:44 -0400

    Refactor some async stuff.

--- tests/index.js
+++ tests/index.js
@@ -3,11 +3,11 @@
  */
 var async = require('async');
 var crypto = require('crypto');
-var events = require('events');
 var program = require('commander');
 var payswarm = require('payswarm-client');
 var winston = require('winston');
 var util = require('util');
+var payswarmTools = require('payswarm.tools');
 
 var loadTester = {};
 config = {};
@@ -33,142 +33,107 @@ loadTester.run = function() {
     .option('--profiles <num>',
       'The number of profiles to create (default: 1).', Number)
     .option('--listings <num>',
-      'The number of listings to use when running the tests (default: 1)', 
+      'The number of listings to use when running the tests (default: 1)',
       Number)
     .option('--purchases <num>',
       'The number of purchases to perform per profile/identity (default: 1)',
       Number)
     .parse(process.argv);
 
-  // initialize the configuration 
+  // initialize the configuration
   config.profiles = program.profiles || 1;
   config.listings = program.listings || 1;
+  // FIXME: impose limits?
+  //config.listings = Math.min(25, program.listings);
   config.purchases = program.purchases || 1;
-  config.vendorKey = 
+  config.vendorKey =
 
   // dump out the configuration
   logger.info('Config:', config);
 
-  // create the listings array
-  var listingsArr = [];
-  for(var i = 0; i < config.listings; i++) {
-    listingsArr.push(i)
-  }
-  
-  // create the profiles array
-  var profilesArr = [];
-  for(var i = 0; i < config.profiles; i++) {
-    profilesArr.push(i)
-  }
-  
-  async.map(listingsArr, 
-    // generate all of the random assetIds
-    function(item, callback) {
-      crypto.randomBytes(4, function(err, buf) {
-        var assetId = buf.toString('hex');
-        callback(null, assetId);
-      });
+  var profiles = [];
+  var listings = [];
+
+  async.auto({
+    createProfiles: function(callback) {
+      logger.info(util.format('Creating %d profiles...', config.profiles));
+      async.forEach(new Array(config.profiles), function(item, callback) {
+        _createProfile(profiles, callback);
+      }, callback);
     },
-    // after all random assetIds are generated, perform profile/listing creation
-    function(err, results) {
-      logger.info('assetIDs:', results);
-      
-      // convert the previous step's results into an array of asset IDs
-      var assetIds = [];
-      for(k in results) {
-        assetIds.push(results[k]);
+    createListings: function(callback) {
+      logger.info(util.format('Creating %d listings...', config.listings));
+      async.forEach(new Array(config.listings), function(item, callback) {
+        _createListing(listings, callback);
+      }, callback);
+    },
+    performPurchases: ['createProfiles', 'createListings',
+      function(callback, results) {
+        logger.debug('profiles', profiles);
+        logger.debug('listings', listings);
+        logger.info(util.format(
+          'Performing %d purchases...', config.purchases));
       }
-      
-      // create all profiles and listings
-      async.auto({
-        createProfiles: function(callback) {
-          logger.info(
-            util.format('Creating %d profiles...', config.profiles));
-
-          // Create P new profiles in batches
-          async.forEachLimit(profilesArr, 25, 
-            loadTester.createProfile,
-            function(err) {
-              if(!err) {
-                callback(null, true); 
-              }
-              else {
-                logger.error('Failed to create all profiles', err);
-              }
-            }
-          );
-        },
-        createListings: function(callback) {
-          logger.info(
-            util.format('Creating %d listings...', config.listings));
-
-          // Create L new listings in batches
-          async.forEachLimit(assetIds, 25, 
-            loadTester.createListing, 
-            function(err) {
-              if(!err) {
-                callback(null, true); 
-              }
-              else {
-                logger.error('Failed to create all listings', err);
-              }
-            }
-          );
-        },
-        performPurchases: ['createProfiles', 'createListings', 
-          // perform the purchases after profiles and listings are created
-          function(callback, results) {
-            logger.info('createProfileResults:', results.createProfiles);
-            logger.info('createListingsResults:', results.createListings);
-            if(results.createProfiles && results.createListings) {
-              logger.info(
-                util.format('Performing %d purchases...', config.purchases));
-            }
-          }
-        ]
-      });
+    ]
+  }, function(err) {
+    if(err) {
+      logger.error('Error', err);
     }
-  );
+  });
 };
 
-loadTester.createProfile = function(item, profileReadyCallback) {
-  var profile = {};
-  
-  logger.info(util.format('Profile: %j', profile));
-  
-  profileReadyCallback();
-},
+// run the program
+loadTester.run();
 
-loadTester.createListing = function(item, listingReadyCallback) {
-  var assetUrl = 
-    'http://listings.dev.payswarm.com/payswarm-auth-tests/' + item + "#asset";
-  var listingUrl = 
-    'http://listings.dev.payswarm.com/payswarm-auth-tests/' + item + "#listing";
+/**
+ * Creates a profile.
+ *
+ * @param callback(err) called once the operation completes.
+ */
+function _createProfile(callback) {
+  var profile = {};
+
+  logger.info(util.format('Profile: %j', profile));
+  profiles.push(profile);
+
+  callback(null);
+}
+
+/**
+ * Creates a listing.
+ *
+ * @param callback(err, listing) called once the operation completes.
+ */
+function _createListing(callback) {
+  var uuid = payswarmTools.uuid();
+  var baseUrl = 'http://listings.dev.payswarm.com/payswarm-auth-tests/' + uuid;
+  var assetId = baseUrl + '#asset';
+  var listingId = baseUrl + '#listing';
 
   // generate the asset
   var asset = {
-    id: assetUrl,
+    id: assetId,
     type: ['ps:Asset', 'pto:WebPage'],
     creator: {
       fullName: 'PaySwarm Test Software'
     },
-    title : 'Test Asset ' + item,
-    assetContent: assetUrl,
+    title : 'Test Asset ' + uuid,
+    assetContent: assetId,
     assetProvider: "https://payswarm.dev:19443/i/vendor",
   };
 
   // generate the listing
   var listing = {
-    id: listingUrl,
+    id: listingId,
     type: ['ps:Listing', 'gr:Offering'],
     payee: [{
-      id: listingUrl + '-payee',
+      id: listingId + '-payee',
       type: 'com:Payee',
       destination: 'https://payswarm.dev:19443/i/vendor/accounts/primary',
-      payeePosition: '0',
+      payeePosition: 0,
       payeeRate: '0.0500000',
       payeeRateType: 'com:FlatAmount',
-      comment: 'Payment for Asset ' + item + '.'
+      comment: 'Payment for Asset ' + uuid + '.'
     }],
     payeeRule : [{
       type: 'com:PayeeRule',
@@ -177,39 +142,30 @@ loadTester.createListing = function(item, listingReadyCallback) {
       payeeRateContext: ['com:Inclusive', 'com:Tax', 'com:TaxExempt'],
       payeeRateType: 'com:Percentage'
     }],
-    asset: assetUrl,
+    asset: assetId,
     assetHash: '',
     license: 'http://purl.org/payswarm/licenses/blogging',
     licenseHash: 'ad8f72fcb47e867231d957c0bffb4c02d275926a',
     validFrom: '2012-07-05T20:35:21+00:00',
     validUntil: '2012-07-06T20:35:21+00:00',
-  }
+  };
   logger.info(util.format('Listing: %j', listing));
-  /*
+
   async.waterfall([
     function(callback) {
       payswarm.hash(asset, callback);
     },
     function(assetHash, callback) {
-       listing.assetHash = assetHash;
-       payswarm.sign(listing, callback);
-    }],
-    function(err, result) {
-      if(!err) {
-        listingReadyCallback();
-      }
-      else
-      {
-        logger.error('Failed to generate listing:', err);
-      }
+      listing.assetHash = assetHash;
+      payswarm.sign(listing, callback);
     }
-  );
-  */
-
-  // notify async that the next item should be processed
-  listingReadyCallback();
+  ], function(err, result) {
+    if(err) {
+      logger.error('Failed to generate listing:', err);
+    }
+    else {
+      listings.push(listing);
+    }
+    callback(err);
+  });
 }
-
-// run the program
-loadTester.run();
-

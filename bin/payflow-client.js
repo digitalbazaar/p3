b357ba98733912ff0883cc12b32bd17d390ebe51
Dave Longley <dlongley@digitalbazaar.com>
2012-07-17 16:50:42 -0400

    Add API calls to verify/charge payment sources.

--- bin/payflow-client.js
+++ bin/payflow-client.js
@@ -12,6 +12,11 @@ program
   .option('--auth <filename>', 'The JSON file containing gateway credentials.')
   .option('--request <filename>', 'A JSON file containing a request to ' +
     'send to the Payflow Gateway.')
+  .option('--verify <filename>', 'A JSON-LD file containing credit card ' +
+    'or bank account information that is to be verified. A payment token ' +
+    'is output.')
+  .option('--charge <filename>', 'A JSON-LD file containing a payment ' +
+    'token to charge. The "--amount" parameter must also be specified.')
   .option('--timeout <timeout>', 'The request timeout in seconds.', parseInt)
   .option('--no-review', 'Do not require confirmation before sending ' +
     'the request.')
@@ -20,14 +25,40 @@ program
   .option('--debug', 'Write debug output to the console.')
   .parse(process.argv);
 
-// check required arguments
+// validate arguments
 if(!program.auth) {
   console.log('\nError: Missing required option "--auth".');
   process.stdout.write(program.helpInformation());
   process.exit(1);
 }
-if(!program.request) {
-  console.log('\nError: Missing required option "--request".');
+if(!program.request && !program.verify && !program.charge) {
+  console.log('\nError: Missing required option ' +
+    '"--request" or "--verify" or "--charge".');
+  process.stdout.write(program.helpInformation());
+  process.exit(1);
+}
+if(program.request && program.verify) {
+  console.log('\nError: Incompatible options "--request" and "--verify".');
+  process.stdout.write(program.helpInformation());
+  process.exit(1);
+}
+if(program.request && program.charge) {
+  console.log('\nError: Incompatible options "--request" and "--charge".');
+  process.stdout.write(program.helpInformation());
+  process.exit(1);
+}
+if(program.request && program.amount) {
+  console.log('\nError: Incompatible options "--request" and "--amount".');
+  process.stdout.write(program.helpInformation());
+  process.exit(1);
+}
+if(program.verify && program.amount) {
+  console.log('\nError: Incompatible options "--verify" and "--amount".');
+  process.stdout.write(program.helpInformation());
+  process.exit(1);
+}
+if(program.charge && !program.amount) {
+  console.log('\nError: You must provide an "--amount" with "--charge".');
   process.stdout.write(program.helpInformation());
   process.exit(1);
 }
@@ -48,31 +79,51 @@ if(config.mode === 'live') {
 }
 
 console.log('\nPayflow Client:\n');
+var client;
+var source;
 
-async.auto({
-  getAuth: function(callback) {
+async.waterfall([
+  function(callback) {
     try {
       var auth = JSON.parse(fs.readFileSync(program.auth, 'utf8'));
-      callback(null, auth);
+      client = new PayflowClient({
+        mode: config.mode,
+        timeout: config.timeout,
+        user: auth.user,
+        vendor: auth.vendor,
+        partner: auth.partner,
+        password: auth.password,
+        debug: config.debug
+      });
+      callback();
     }
     catch(ex) {
       callback(ex);
     }
   },
-  getRequest: function(callback) {
+  function(callback) {
+    var filename = program.request || program.verify || program.charge;
     try {
-      var request = JSON.parse(fs.readFileSync(program.request, 'utf8'));
-      callback(null, request);
+      var data = JSON.parse(fs.readFileSync(filename, 'utf8'));
+      if(program.request) {
+        return callback(null, data);
+      }
+      if(program.verify) {
+        source = data;
+        return callback(null, client.createVerifyRequest(data));
+      }
+      if(program.charge) {
+        return callback(null, client.createChargeRequest(data));
+      }
     }
     catch(ex) {
-      callback(ex);
+      return callback(ex);
     }
   },
-  confirm: ['getAuth', 'getRequest', function(callback, results) {
+  function(callback, request) {
     if(!config.confirm) {
-      return callback();
+      return callback(null, request);
     }
-    var request = results.getRequest;
     console.log('Mode: ' + config.mode);
     console.log('Timeout: ' + config.timeout);
     console.log('Request: ', JSON.stringify(request, null, 2) + '\n');
@@ -83,26 +134,23 @@ async.auto({
       }
       callback();
     });
-  }],
-  sendRequest: ['confirm', function(callback, results) {
+  },
+  function(callback, request) {
     console.log('\nSending request...');
-    var auth = results.getAuth;
-    var request = results.getRequest;
-    var client = new PayflowClient({
-      mode: config.mode,
-      timeout: config.timeout,
-      user: auth.user,
-      vendor: auth.vendor,
-      partner: auth.partner,
-      password: auth.password,
-      debug: config.debug
-    });
     client.send(request, callback);
-  }]
-}, function(err) {
+  },
+  function(callback, response) {
+    if(program.verify) {
+      response.paymentToken = client.createPaymentToken(source, response);
+    }
+    callback(null, response);
+  }
+], function(err) {
   if(err) {
     console.log('\n' + err, err.stack ? err.stack : '');
     process.exit(1);
   }
+  console.log('Response:');
+  console.log(JSON.stringify(response, null, 2));
   process.exit();
 });

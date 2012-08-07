74ef7117e0e49251451098903034c6713f9bf758
Dave Longley <dlongley@digitalbazaar.com>
2012-08-07 17:35:35 -0400

    Use Array.forEach().

--- email-templates/mapper.js
+++ email-templates/mapper.js
@@ -18,12 +18,11 @@ module.exports.map = function(mapping) {
 
   // FIXME: can't just map to filenames because swig can't use more than
   // one root directory, so the files must be loaded manually here
-  for(var idx in ids) {
-    var id = ids[idx];
+  ids.forEach(function(id) {
     var filename = __dirname + '/' + id + '.tpl';
     mapping[id] = {
       template: fs.readFileSync(filename).toString('utf8'),
       filename: filename
     };
-  }
+  });
 };

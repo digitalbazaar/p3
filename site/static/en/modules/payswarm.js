6afe0c3c4a2774a6ec4af066ccb2e50d5281d9e9
Dave Longley <dlongley@digitalbazaar.com>
2012-12-05 14:37:34 -0500

    Define console.log when undefined (for IE).

--- site/static/en/modules/payswarm.js
+++ site/static/en/modules/payswarm.js
@@ -5,6 +5,10 @@
  */
 (function() {
 
+// define console.log for IE
+window.console = window.console || {};
+window.console.log = window.console.log || angular.noop();
+
 angular.module('payswarm.services', []);
 angular.module('payswarm.directives', ['payswarm.services']);
 angular.module('payswarm.filters', []);

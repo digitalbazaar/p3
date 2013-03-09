5624ed2ad1b0c20a01062d0a5c4ff1005152d7d1
Manu Sporny <msporny@digitalbazaar.com>
2013-03-09 17:18:53 -0500

    Fixed jscoverage and generating HTML file of code coverage.

--- test.js
+++ test.js
@@ -5,7 +5,6 @@ var path = require('path');
 __libdir = process.env['PAYSWARM_AUTH_COV'] ?
   path.resolve(__dirname, 'lib-cov') :
   path.resolve(__dirname, 'lib');
-top = {};
 var pa = require(__libdir + '/payswarm-auth');
 
 // load test config and start

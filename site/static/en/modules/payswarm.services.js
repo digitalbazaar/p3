11a5fa1beea5b3a6c1ada6a73fa96cc43dc197d9
Dave Longley <dlongley@digitalbazaar.com>
2013-08-10 13:59:58 -0400

    Fix some lint stuff.

--- site/static/en/modules/payswarm.services.js
+++ site/static/en/modules/payswarm.services.js
@@ -1609,8 +1609,8 @@ angular.module('payswarm.services')
         modal.$backdrop.click(function(event) {
           // only focus/hide if the click is on the backdrop itself
           if(event.target === modal.$backdrop[0]) {
-            (modal.options.backdrop === 'static') ?
-              element.focus() : modal.hide();
+            (modal.options.backdrop === 'static' ?
+              element.focus() : modal.hide());
           }
         });
       }
--- test.js
+++ test.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2012 Digital Bazaar, Inc. All rights reserved.
  */
 var path = require('path');
-__libdir = process.env['PAYSWARM_AUTH_COV'] ?
+__libdir = process.env.PAYSWARM_AUTH_COV ?
   path.resolve(__dirname, 'lib-cov') :
   path.resolve(__dirname, 'lib');
 var pa = require(__libdir + '/payswarm-auth');

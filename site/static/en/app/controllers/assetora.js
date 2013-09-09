21ef1e9bbf6d455690e416cdf98e5866369dee58
David I. Lehn <dlehn@digitalbazaar.com>
2013-09-09 18:04:31 -0400

    Remove code that moved elsewhere.

--- site/static/en/app/controllers/assetora.js
+++ site/static/en/app/controllers/assetora.js
@@ -14,25 +14,6 @@ return {
       templateUrl: '/partials/tools/assetora.html',
       controller: 'AssetoraCtrl'
     }
-  // FIXME: move other routes to their respective controller files
-  }, {
-    path: '/i/:identity/invoices',
-    options: {
-      templateUrl: '/partials/tools/invoices.html',
-      controller: 'InvoicesCtrl'
-    }
-  }, {
-    path: '/i/:identity/causes',
-    options: {
-      templateUrl: '/partials/tools/causes.html',
-      controller: 'CausesCtrl'
-    }
-  }, {
-    path: '/i/:identity/tickets',
-    options: {
-      templateUrl: '/partials/tools/tickets.html',
-      controller: 'TicketsCtrl'
-    }
   }]
 };
 
@@ -143,95 +124,6 @@ function factory($scope, svcHostedAsset, svcHostedListing, $timeout) {
 
   svcHostedAsset.getRecent({force: true});
   svcHostedListing.getRecent({force: true});
-}
-
-// FIXME: move to separate file
-function InvoicesCtrl($scope, svcHostedAsset, svcHostedListing) {
-  $scope.model = {};
-  // FIXME: globalize window.data access
-  var data = window.data || {};
-  $scope.identity = data.identity;
-  $scope.model.recentAssets = svcHostedAsset.recentAssets;
-  $scope.state = {
-    assets: svcHostedAsset.state
-  };
-  $scope.model.search = {input: '', assets: []};
-  $scope.model.modals = {
-    asset: null,
-    item: null,
-    showAddInvoice: false,
-    showEditInvoice: false,
-    showAddInvoiceItem: false,
-    showEditInvoiceItem: false
-  };
-  $scope.deleteInvoice = function(invoice) {
-    $scope.showDeleteInvoiceAlert = true;
-    $scope.invoiceToDelete = invoice;
-  };
-  $scope.confirmDeleteInvoice = function(err, result) {
-    // FIXME: handle errors
-    if(!err && result === 'ok') {
-      var invoice = $scope.invoiceToDelete;
-      invoice.deleted = true;
-
-      // wait to delete so modal can transition
-      $timeout(function() {
-        svcAsset.del(asset.id, function(err) {
-          if(err) {
-            asset.deleted = false;
-          }
-        });
-        // FIXME: delete related listing
-      }, 400);
-    }
-    $scope.assetToDelete = null;
-  };
-  $scope.search = function(input, state, callback) {
-    if(input.length === 0) {
-      $scope.model.search.assets.splice(
-        0, $scope.model.search.assets.length);
-      return;
-    }
-
-    // FIXME: remove me
-    console.log('search', input, state);
-
-    // FIXME: have toggle for searching assets vs. listings, kinda messy ...
-    // would be better to just have search results return assets and listings
-    // together?
-
-    // search listings for input as keywords
-    svcHostedAsset.get({
-      storage: $scope.model.search.assets,
-      keywords: $scope.model.search.input
-    }, function(err) {
-      if(err) {
-        state.error = err;
-      }
-      else {
-        state.error = null;
-      }
-      callback();
-    });
-  };
-
-  svcHostedAsset.getRecent({force: true});
-}
-
-// FIXME: move to separate file
-function CausesCtrl($scope, svcHostedAsset, svcHostedListing) {
-  $scope.model = {};
-  // FIXME: globalize window.data access
-  var data = window.data || {};
-  $scope.identity = data.identity;
-}
-
-// FIXME: move to separate file
-function TicketsCtrl($scope, svcHostedAsset, svcHostedListing) {
-  $scope.model = {};
-  // FIXME: globalize window.data access
-  var data = window.data || {};
-  $scope.identity = data.identity;
 }
 
 });

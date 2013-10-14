53d699d9e9730f9a45a030396f569219fb0691f5
Dave Longley <dlongley@digitalbazaar.com>
2013-10-14 16:10:49 -0400

    Various bug fixes to enable deposits and contracts.

--- schemas/depositAmount.js
+++ schemas/depositAmount.js
@@ -5,7 +5,7 @@ var schema = {
   title: 'Deposit Amount',
   description: 'An amount to be deposited.',
   type: 'string',
-  pattern: '^([1-9]{1}[0-9]{0,2}(\\,[0-9]{3})*(\\.[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\\.[0-9]{0,2})?)$',
+  pattern: '^([0-9]{1}[0-9]{0,2}(\\,[0-9]{3})*(\\.[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\\.[0-9]{0,2})?)$',
   errors: {
     invalid: 'The deposit amount is invalid.',
     missing: 'Please deposit a minimum of USD $1.00.'

4525b93c2dc688bd0462652ea567d3586337dae1
David I. Lehn <dlehn@digitalbazaar.com>
2013-11-11 22:27:41 -0500

    Support omitting selectable tokens.
    - Add a directive option to provide a list of ids of tokens to not
      display.
    - Can be used to avoid showing tokens already in a token list.

--- site/static/en/app/directives/paymentTokenSelector.js
+++ site/static/en/app/directives/paymentTokenSelector.js
@@ -5,41 +5,70 @@
  */
 define([], function() {
 
-var deps = ['svcPaymentToken'];
+var deps = ['svcPaymentToken', 'svcModel'];
 return {paymentTokenSelector: deps.concat(factory)};
 
-function factory(svcPaymentToken) {
+function factory(svcPaymentToken, svcModel) {
   function Ctrl($scope) {
     $scope.model = {};
     $scope.services = {
       token: svcPaymentToken.state
     };
-    $scope.paymentTokens = svcPaymentToken.paymentTokens;
+    $scope.paymentTokens = [];
+    $scope.tokensFilteredByInstant = [];
     $scope.$watch('paymentTokens', function(tokens) {
       if(!$scope.selected || $.inArray($scope.selected, tokens) === -1) {
         $scope.selected = tokens[0] || null;
       }
     }, true);
-    svcPaymentToken.get();
+    svcPaymentToken.get(function(err, tokens) {
+      if(!err) {
+        filterTokens($scope);
+      }
+    });
+  }
+
+  function filterTokens(scope) {
+    // omit tokens if needed
+    var filteredByInstant = scope.tokensFilteredByInstant;
+    var omit = scope.omit || [];
+    var tokens = [];
+    for(var ti = 0; ti < filteredByInstant.length; ++ti) {
+      var token = filteredByInstant[ti];
+      var skip = false;
+      for(var oi = 0; oi < omit.length && !skip; ++oi) {
+        skip = (token.id === omit[oi]);
+      }
+      if(!skip) {
+        tokens.push(token);
+      }
+    }
+    svcModel.replaceArray(scope.paymentTokens, tokens);
   }
 
   function Link(scope, element, attrs) {
     attrs.$observe('fixed', function(value) {
       scope.fixed = value;
     });
+
     scope.$watch('instant', function(value) {
       if(value === 'non') {
-        scope.paymentTokens = svcPaymentToken.nonInstant;
+        scope.tokensFilteredByInstant = svcPaymentToken.nonInstant;
         scope.paymentMethods = svcPaymentToken.nonInstantPaymentMethods;
       }
       else if(value) {
-        scope.paymentTokens = svcPaymentToken.instant;
+        scope.tokensFilteredByInstant = svcPaymentToken.instant;
         scope.paymentMethods = svcPaymentToken.instantPaymentMethods;
       }
       else {
-        scope.paymentTokens = svcPaymentToken.paymentTokens;
+        scope.tokensFilteredByInstant = svcPaymentToken.paymentTokens;
         scope.paymentMethods = svcPaymentToken.paymentMethods;
       }
+      filterTokens(scope);
+    });
+
+    scope.$watch('omit', function(value) {
+      filterTokens(scope);
     });
   }
 
@@ -48,7 +77,8 @@ function factory(svcPaymentToken) {
       selected: '=',
       invalid: '=',
       fixed: '@',
-      instant: '='
+      instant: '=',
+      omit: '='
     },
     controller: ['$scope', Ctrl],
     templateUrl: '/partials/payment-token-selector.html',

9aa8c09827ab54ab7a5dd61dfa94380e1d2e7be3
Dave Longley <dlongley@digitalbazaar.com>
2013-11-26 13:57:54 -0500

    Angularify budget bar directive.

--- site/static/en/app/directives/budgetBar.js
+++ site/static/en/app/directives/budgetBar.js
@@ -16,39 +16,19 @@ function factory() {
     replace: true,
     templateUrl: '/app/templates/budget-bar.html',
     controller: ['$scope', function($scope) {
+      var model = $scope.model = {};
+      model.barPercentage = 0;
+      model.textPercentage = 0;
     }],
     link: function(scope, element, attrs) {
-      var progress = $('.progress', element);
-      var bar = $('.bar', progress);
-      var frontText = $('.progressbar-text-front', bar);
-
       // update progress bar when balance or amount changes
       scope.$watch('budget', function(budget) {
-        var class_;
+        var model = scope.model;
         var balance = budget ? budget.balance : '0';
         var amount = budget ? budget.amount : '0';
-        var p = parseFloat(balance) / parseFloat(amount) * 100;
-        p = Math.max(0, Math.min(p, 100));
-        if(p < 25) {
-          class_ = 'progress-danger';
-        }
-        else if(p < 50) {
-          class_ = 'progress-warning';
-        }
-        else {
-          class_ = 'progress-success';
-        }
-        progress
-          .removeClass('progress-danger')
-          .removeClass('progress-info')
-          .removeClass('progress-success')
-          .addClass(class_);
-
-        // update actual bar
-        bar.attr('data-amount-part', balance);
-        bar.attr('data-amount-total', amount);
-        bar.css('width', p + '%');
-        frontText.css('width', 100 / p * 100 + '%');
+        model.barPercentage = parseFloat(balance) / parseFloat(amount) * 100;
+        model.barPercentage = Math.max(0, Math.min(model.barPercentage, 100));
+        model.textPercentage = 100 / model.barPercentage * 100;
       }, true);
     }
   };

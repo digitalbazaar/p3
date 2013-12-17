e4f4b30e211eb80ec026099bc0e4a614edf7f9c3
Dave Longley <dlongley@digitalbazaar.com>
2013-12-17 12:20:21 -0500

    Split account balance directive into summary+details.
    - Allows for more flexible display of component parts of the widget.

---
+++ site/static/en/app/directives/accountBalanceDetails.js
@@ -0,0 +1,78 @@
+/*!
+ * Account Balance Details directive. To be paired w/Account Balance Summary.
+ *
+ * @author Dave Longley
+ */
+define([], function() {
+
+var deps = ['svcPaymentToken'];
+return {accountBalanceDetails: deps.concat(factory)};
+
+function factory(svcPaymentToken) {
+  return {
+    scope: {
+      account: '=accountBalanceDetails',
+      expand: '='
+    },
+    replace: true,
+    templateUrl: '/app/templates/account-balance-details.html',
+    controller: ['$scope', function($scope) {
+      var model = $scope.model = {};
+      model.paymentMethodIsCollapsed = true;
+      model.creditBar = {
+        width: 0,
+        textWidth: 0
+      };
+      model.hasCreditLine = ($scope.account ?
+        parseFloat($scope.account.creditLimit || '0') > 0 : false);
+    }],
+    link: function(scope, element, attrs) {
+      var model = scope.model;
+
+      // update progress bar when account changes
+      scope.$watch('account', function(account) {
+        // get balance and credit limit
+        model.balance = parseFloat(account ? account.balance : 0);
+        model.creditLimit = parseFloat(
+          account ? account.creditLimit || '0' : 0);
+
+        // calculate remaining credit
+        model.remainingCredit = model.creditLimit;
+        if(model.balance < 0) {
+          model.remainingCredit = model.creditLimit + model.balance;
+        }
+
+        // get total balance (includes remaining credit)
+        model.totalBalance = model.remainingCredit;
+        if(model.balance > 0) {
+          model.totalBalance += model.balance;
+        }
+
+        // credit info display
+        model.hasCreditLine = (model.creditLimit > 0);
+        if(model.hasCreditLine) {
+          // get backup source token
+          model.backupSource = null;
+          if(account.backupSource && account.backupSource.length) {
+            svcPaymentToken.getOne(
+              account.backupSource[0], function(err, token) {
+                model.backupSource = token;
+              });
+          }
+
+          // credit bar width
+          if(model.balance >= 0) {
+            model.creditBar.width = 100;
+          }
+          else {
+            var p = model.remainingCredit / model.creditLimit * 100;
+            model.creditBar.width = Math.max(0, Math.min(p, 100));
+          }
+        }
+        model.creditBar.textWidth = 100 / model.creditBar.width * 100;
+      }, true);
+    }
+  };
+}
+
+});

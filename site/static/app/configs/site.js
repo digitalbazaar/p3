8645cb1fe48c812c7f149f29982a36bab4edbcac
David I. Lehn <dlehn@digitalbazaar.com>
2014-06-20 13:15:09 -0400

    Move tools navbar submenu into site config.
    - Use Bedrock feature for submenu specification.
    - Add submenu to site.js config.
    - Remove navbar-private.html.

--- site/static/app/configs/site.js
+++ site/static/app/configs/site.js
@@ -19,6 +19,48 @@ return {
           label: 'Dashboard',
           pageTitle: 'Dashboard'
         },
+/*
+        {
+          slug: 'tools',
+          icon: 'icon-briefcase',
+          label: 'Tools',
+          items: [
+            {
+              slug: 'assetora',
+              icon: 'icon-cloud',
+              label: 'Sell Digital Content',
+              pageTitle: 'Sell Digital Content'
+            },
+            {
+              slug: 'invoices',
+              icon: 'icon-money',
+              label: 'Invoices',
+              pageTitle: 'Invoices'
+            },
+            {
+              slug: 'causes',
+              icon: 'icon-heart',
+              label: 'Causes',
+              pageTitle: 'Causes'
+            },
+            {
+              slug: 'tickets',
+              icon: 'icon-ticket',
+              label: 'Tickets',
+              pageTitle: 'Tickets'
+            },
+            {
+              divider: true
+            },
+            {
+              slug: 'tools',
+              icon: 'icon-list',
+              label: 'More',
+              pageTitle: 'Tools'
+            }
+          ]
+        },
+*/
         {
           slug: 'settings',
           icon: 'icon-wrench',

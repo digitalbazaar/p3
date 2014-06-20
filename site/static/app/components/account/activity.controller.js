ccc0596d05bc9c2f196982d895168dc5e41fbafe
David I. Lehn <dlehn@digitalbazaar.com>
2014-06-20 14:51:00 -0400

    Change model to self and remove unused error var.

--- site/static/app/components/account/activity.controller.js
+++ site/static/app/components/account/activity.controller.js
@@ -13,82 +13,81 @@ return {ActivityCtrl: deps.concat(factory)};
 function factory(
   $timeout, config,
   svcAlert, svcIdentity, svcResource, svcTransaction) {
-  var model = this;
-  model.identity = svcIdentity.identity;
-  model.session = config.data.session || null;
-  model.account = config.data.account || null;
-  model.txns = new svcResource.Collection({
+  var self = this;
+  self.identity = svcIdentity.identity;
+  self.session = config.data.session || null;
+  self.account = config.data.account || null;
+  self.txns = new svcResource.Collection({
     url: '/transactions',
     finishLoading: _updateTable
   });
   _updateTable();
-  model.error = null;
-  model.datePickerOpen = false;
+  self.datePickerOpen = false;
 
   // set start date to last ms of today
   var now = new Date();
-  model.startDate = new Date(
+  self.startDate = new Date(
     now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 999);
-  model.datePickerDate = new Date(model.startDate);
+  self.datePickerDate = new Date(self.startDate);
 
-  // convert date into the last millisecond of the day, also separate model
+  // convert date into the last millisecond of the day, also separate
   // vars are used to avoid modifying the text while typing and to ensure the
   // input blurs when using the calendar selector
-  model.dateChanged = function() {
-    if(model.datePickerDate) {
-      model.startDate = new Date(
-        model.datePickerDate.getFullYear(),
-        model.datePickerDate.getMonth(),
-        model.datePickerDate.getDate(), 23, 59, 59, 999);
-      model.txns = new svcResource.Collection({
+  self.dateChanged = function() {
+    if(self.datePickerDate) {
+      self.startDate = new Date(
+        self.datePickerDate.getFullYear(),
+        self.datePickerDate.getMonth(),
+        self.datePickerDate.getDate(), 23, 59, 59, 999);
+      self.txns = new svcResource.Collection({
         url: '/transactions',
         finishLoading: _updateTable
       });
-      model.getMore();
+      self.getMore();
     }
   };
 
   // lose focus and hide datepicker
-  model.dateQuitKeyPressed = function($event) {
-    model.datePickerOpen = false;
+  self.dateQuitKeyPressed = function($event) {
+    self.datePickerOpen = false;
     $event.target.blur();
   };
 
-  model.openDatePicker = function() {
-    var isOpen = model.datePickerOpen;
+  self.openDatePicker = function() {
+    var isOpen = self.datePickerOpen;
     $timeout(function() {
-      model.datePickerOpen = !isOpen;
+      self.datePickerOpen = !isOpen;
     });
   };
 
-  model.getRowType = svcTransaction.getType;
+  self.getRowType = svcTransaction.getType;
 
-  model.getMore = function() {
+  self.getMore = function() {
     // build options for fetching txns
     var params = {};
-    if(model.account) {
-      params.account = model.account.id;
+    if(self.account) {
+      params.account = self.account.id;
     }
     // previous txns exist, get next page
-    if(model.txns.storage.length > 0) {
-      var txn = model.txns.storage[model.txns.storage.length - 1];
+    if(self.txns.storage.length > 0) {
+      var txn = self.txns.storage[self.txns.storage.length - 1];
       params.createdStart = txn.created;
       params.previous = txn.id;
     } else {
-      params.createdStart = model.startDate;
+      params.createdStart = self.startDate;
     }
     if(params.createdStart instanceof Date) {
       params.createdStart = (+params.createdStart / 1000);
     }
 
     // fetch txns
-    model.txns.getAll({params: params}).catch(function(err) {
+    self.txns.getAll({params: params}).catch(function(err) {
       svcAlert.addError(err);
     });
   };
 
   // show/hide transaction details
-  model.toggleDetails = function(row) {
+  self.toggleDetails = function(row) {
     var txn = row;
     if(row.type.indexOf('Transfer') !== -1) {
       txn = row.txn;
@@ -100,18 +99,18 @@ function factory(
   };
 
   // populate table with first set of txns
-  model.getMore();
+  self.getMore();
 
   function _updateTable() {
-    model.table = [];
-    angular.forEach(model.txns.storage, function(txn) {
+    self.table = [];
+    angular.forEach(self.txns.storage, function(txn) {
       // skip txns w/insufficent funds
       if(txn.voided &&
         txn.voidReason === 'payswarm.financial.InsufficientFunds') {
         return;
       }
 
-      model.table.push(txn);
+      self.table.push(txn);
       angular.forEach(txn.transfer, function(transfer) {
         transfer.txn = txn;
         transfer.sourceLink = true;
@@ -120,7 +119,7 @@ function factory(
           var src = txn.source;
           transfer.sourceLink = false;
           transfer.source = src.label;
-          if(src.owner === model.identity.id) {
+          if(src.owner === self.identity.id) {
             if(src.paymentMethod === 'CreditCard') {
               transfer.source += ' (Credit Card: ' + src.cardNumber + ')';
             } else if(src.paymentMethod === 'BankAccount') {
@@ -132,14 +131,14 @@ function factory(
           var dst = txn.destination;
           transfer.destinationLink = false;
           transfer.destination = dst.label;
-          if(dst.owner === model.identity.id &&
+          if(dst.owner === self.identity.id &&
             dst.paymentMethod === 'BankAccount') {
             transfer.destination +=
               ' (Bank Account: ' + dst.bankAccount + ')';
           }
         }
         transfer.hidden = true;
-        model.table.push(transfer);
+        self.table.push(transfer);
       });
     });
   }

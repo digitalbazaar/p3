14fe9eadd1b52a186daf860403a051d95823dac1
Dave Longley <dlongley@digitalbazaar.com>
2014-06-25 16:42:21 -0400

    Bedrock-update update-account-button-directive.

--- site/static/app/components/account/update-account-button-directive.js
+++ site/static/app/components/account/update-account-button-directive.js
@@ -3,50 +3,40 @@
  *
  * @author Dave Longley
  */
-define(['payswarm.api'], function(payswarm) {
+define([], function() {
 
-var deps = ['AccountService'];
+var deps = ['AccountService', 'AlertService', 'IdentityService', 'config'];
 return {updateAccountButton: deps.concat(factory)};
 
-function factory(AccountService) {
-  function Ctrl($scope) {
-    $scope.data = window.data || {};
-    $scope.feedback = {};
+function factory(AccountService, AlertService, IdentityService, config) {
+  return {
+    scope: {account: '=', callback: '&'},
+    templateUrl: '/app/components/account/update-account-button.html',
+    link: Link
+  };
 
-    var model = $scope.model = {};
-    model.loading = false;
-    model.identity = $scope.data.identity || {};
+  function Link(scope, element) {
+    var model = scope.model = {};
+    model.identity = IdentityService.identity;
 
-    $scope.updateAccount = function() {
+    model.updateAccount = function() {
       // merge in all properties from given account
       var account = {
-        '@context': payswarm.CONTEXT_URL,
-        id: $scope.account.id
+        '@context': config.data.contextUrl,
+        id: scope.account.id
       };
-      $scope.account.forEach(function(property) {
-        account[property] = $scope.account[property];
+      scope.account.forEach(function(property) {
+        account[property] = scope.account[property];
       });
 
-      model.loading = true;
-      AccountService.update(account, function(err, account) {
-        model.loading = false;
-        $scope.feedback.error = err;
-        $scope.callback(err, account);
+      AccountService.update(account).then(function(account) {
+        scope.callback(null, account);
+      }).catch(function(err) {
+        AlertService.add('error', err);
+        scope.callback(err, account);
       });
     };
   }
-
-  return {
-    scope: {
-      account: '=',
-      callback: '&'
-    },
-    templateUrl: '/app/components/account/update-account-button.html',
-    controller: ['$scope', Ctrl],
-    link: function(scope, element) {
-      scope.feedbackTarget = element;
-    }
-  };
 }
 
 });

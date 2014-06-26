144a73bc9f8c33947074ebb2a1e1a539ca0a777d
David I. Lehn <dlehn@digitalbazaar.com>
2014-06-26 17:47:22 -0400

    Load account resources.

--- site/static/app/components/account/account.js
+++ site/static/app/components/account/account.js
@@ -7,9 +7,33 @@
  */
 define([
   'angular',
+  './account-balance-directive',
+  './account-balance-summary-directive',
+  './account-controller',
+  './account-selector-directive',
   './account-service',
-  './activity-controller'
-], function(angular, accountService, activityController) {
+  './activity-controller',
+  './add-account-modal-directive',
+  './add-credit-line-modal-directive',
+  './deposit-modal-directive',
+  './edit-account-modal-directive',
+  './update-account-button-directive',
+  './withdraw-modal-directive'
+], function(
+  angular,
+  accountBalanceDirective,
+  accountBalanceSummaryDirective,
+  accountController,
+  accountSelectorDirective,
+  accountService,
+  activityController,
+  addAccountModalDirective,
+  addCreditLineModalDirective,
+  depositModalDirective,
+  editAccountModalDirective,
+  updateAccountButtonDirective,
+  withdrawModalDirective
+) {
 
 'use strict';
 
@@ -17,6 +41,19 @@ var module = angular.module('app.account', []);
 
 module.service(accountService);
 module.controller(activityController);
+
+module.directive(accountBalanceDirective);
+module.directive(accountBalanceSummaryDirective);
+module.controller(accountController);
+module.directive(accountSelectorDirective);
+module.service(accountService);
+module.controller(activityController);
+module.directive(addAccountModalDirective);
+module.directive(addCreditLineModalDirective);
+module.directive(depositModalDirective);
+module.directive(editAccountModalDirective);
+module.directive(updateAccountButtonDirective);
+module.directive(withdrawModalDirective);
 
 return module.name;
 

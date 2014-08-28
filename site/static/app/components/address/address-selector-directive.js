32de70bbd59fc51ebdadfb99b7731856aaca8cb1
Dave Longley <dlongley@digitalbazaar.com>
2014-08-28 11:59:56 -0400

    Use new selector directive as base.

--- site/static/app/components/address/address-selector-directive.js
+++ site/static/app/components/address/address-selector-directive.js
@@ -8,40 +8,57 @@ define([], function() {
 'use strict';
 
 /* @ngInject */
-function factory(AddressService, IdentityService) {
+function addressSelectorInner(AddressService, IdentityService) {
   return {
     restrict: 'A',
-    scope: {
-      selected: '=psSelected',
-      invalid: '=psInvalid',
-      fixed: '@psFixed'
-    },
-    templateUrl: '/app/components/address/address-selector.html',
+    require: 'brSelector2',
     link: Link
   };
 
-  function Link(scope, element, attrs) {
+  function Link(scope, element, attrs, brSelector) {
     var model = scope.model = {};
     model.services = {
       address: AddressService.state
     };
     model.identity = IdentityService.identity;
     model.addresses = AddressService.addresses;
-
-    attrs.$observe('psFixed', function(value) {
-      scope.fixed = value;
-    });
-
     scope.$watch('model.addresses', function(addresses) {
       if(!scope.selected || $.inArray(scope.selected, addresses) === -1) {
         scope.selected = addresses[0] || null;
       }
     }, true);
 
+    // configure brSelector
+    scope.brSelector = brSelector;
+    brSelector.itemType = 'Address';
+    brSelector.items = model.addresses;
+    brSelector.addItem = function() {
+      model.showAddAddressModal = true;
+    };
+    scope.$watch('fixed', function(value) {
+      brSelector.fixed = value;
+    });
+
     AddressService.collection.getAll();
   }
 }
 
-return {psAddressSelector: factory};
+/* @ngInject */
+function addressSelector() {
+  return {
+    restrict: 'EA',
+    scope: {
+      selected: '=psSelected',
+      invalid: '=psInvalid',
+      fixed: '=?psFixed'
+    },
+    templateUrl: '/app/components/address/address-selector.html'
+  };
+}
+
+return {
+  psAddressSelector: addressSelector,
+  psAddressSelectorInner: addressSelectorInner
+};
 
 });

19bc913d5ce4d48a2444871437b2d59561945b3c
Dave Longley <dlongley@digitalbazaar.com>
2014-09-12 13:48:38 -0400

    Get promo binary in working order, still needs tweaking.

--- bin/promo.js
+++ bin/promo.js
@@ -1,12 +1,14 @@
-GLOBAL.__libdir = require('path').resolve(__dirname, '../lib');
+var path = require('path');
+GLOBAL.__libdir = path.resolve(path.join(
+  __dirname, '..', 'node_modules', 'bedrock', 'lib'));
 var async = require('async');
-var program = require('commander');
+var bedrock = require('bedrock');
 var fs = require('fs');
-var payswarm = {
-  db: require('../lib/payswarm-auth/database'),
-  promo: require('../lib/payswarm-auth/promo'),
-  validation: require('../lib/payswarm-auth/validation')
-};
+
+// FIXME: provide override via command line option
+// FIXME: use config that only loads required modules
+// load PaySwarm config
+require('../configs/payswarm.dev');
 
 var APP_NAME = 'payswarm.apps.PromotionEditor';
 
@@ -17,64 +19,60 @@ var APP_NAME = 'payswarm.apps.PromotionEditor';
   "redeemable": 1,
   "deposit": [{
     "amount": "1.0000000000",
+    "currency": "USD",
     "comment": "The reason for the funds"
   }],
-  "title": "The Promotion Title"
+  "title": "The Promotion Title",
   "description": "The promotion description."
 }
 */
-
-// FIXME: see audit tool to add options like config, etc.
-program
-  .version('0.0.1')
-  .option('--create <filename>', 'The JSON file containing the promotion details.')
-  .parse(process.argv);
-
-if(!program.create) {
-  console.log('\nError: Missing required option "--create".');
-  process.stdout.write(program.helpInformation());
-  process.exit(1);
-}
-
-// log uncaught exception and exit
-process.on('uncaughtException', function(err) {
-  console.log(err.toString(), err.stack ? {stack: err.stack} : null);
-  process.removeAllListeners('uncaughtException');
-  process.exit();
-});
 
 console.log('\nPromotion Editor:\n');
 
 async.waterfall([
   function(callback) {
-    // init database
-    payswarm.db.init(null, callback);
+    // start bedrock
+    bedrock.start({
+      // FIXME: see audit tool to add options like config, etc.
+      program: {
+        version: '0.0.1',
+        options: [
+          '--create <filename>', 'The JSON file containing the promotion details.'
+        ]
+      }
+    }, callback);
   },
-  function(callback) {
-    payswarm.promo.init(null, callback);
-  },
-  function(callback) {
-    if(program.create) {
-      console.log('\nCreating new promotion from file: "' +
-        program.create + '"...');
-      var promo = JSON.parse(fs.readFileSync(program.create, 'utf8'));
-      var schema = require('../schemas/promo')();
-      return payswarm.validation.validateInstance(promo, schema, function(err) {
-        if(err) {
-          return callback(err);
-        }
-        payswarm.promo.createPromo(null, promo, callback);
-      });
+  function(program, callback) {
+    if(!program.create) {
+      console.log('\nError: Missing required option "--create".');
+      process.stdout.write(program.helpInformation());
+      process.exit(1);
     }
-    callback();
+
+    var payswarm = {
+      config: bedrock.module('config'),
+      db: bedrock.module('bedrock.database'),
+      promo: require('../lib/payswarm-auth/promo'),
+      validation: bedrock.module('validation')
+    };
+
+    console.log('\nCreating new promotion from file: "' +
+      program.create + '"...');
+    console.log('foo', fs.readFileSync(program.create, 'utf8'));
+    var promo = JSON.parse(fs.readFileSync(program.create, 'utf8'));
+    var schema = require('../schemas/promo')();
+    return payswarm.validation.validateInstance(promo, schema, function(err) {
+      if(err) {
+        return callback(err);
+      }
+      payswarm.promo.createPromo(null, promo, callback);
+    });
   }
 ], function(err) {
   if(err) {
     console.log('\n' + err, err.stack ? err.stack : '');
     process.exit(1);
   }
-  if(program.create) {
-    console.log('\nPromotion created.');
-  }
+  console.log('\nPromotion created.');
   process.exit();
 });

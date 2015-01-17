6caa60683ab6fd9f79da06f7014955c1cea7fef0
Manu Sporny <msporny@digitalbazaar.com>
2015-01-17 11:59:06 -0500

    Add Yahoo Address Validation example config file.

---
+++ configs/yahoo-av.example.js
@@ -0,0 +1,7 @@
+var config = require('bedrock').config;
+
+// yahoo address validator config
+config.addressValidator.yahoo = {};
+config.addressValidator.yahoo.hashKey = 'YAHOO_AV_HASH_KEY';
+config.addressValidator.yahoo.consumerKey = 'YAHOO_AV_CONSUMER_KEY';
+config.addressValidator.yahoo.consumerSecret = 'YAHOO_AV_CONSUMER_SECRET';
